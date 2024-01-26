97b24581a2ed05d6b07c23159dd5089eaaf191fd
jonyadmed19 <jonyahmed19@gmail.com>
2024-01-26 15:34:42 +0100

    [Done] v2

---
+++ src/api/interview-catagorie/controllers/interview-catagorie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * interview-catagorie controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::interview-catagorie.interview-catagorie');
---
+++ src/api/video-interview/routes/video-interview.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * video-interview router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::video-interview.video-interview');
