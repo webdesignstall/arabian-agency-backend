5ea788764786802131b29e7022950869c0f384eb
jonyadmed19 <jonyahmed19@gmail.com>
2024-01-27 16:00:03 +0100

    [Done] video interview and tecknologies

---
+++ src/api/technologies-section/controllers/technologies-section.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * technologies-section controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::technologies-section.technologies-section');
---
+++ src/api/technologies-section/services/technologies-section.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * technologies-section service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::technologies-section.technologies-section');
